a0e4dcd04693f62e485c8e55e7320f5586a9e44e
Al <87392984+LiShanDa2021@users.noreply.github.com>
2022-02-28 20:29:17 -0600

    see if this works

--- app/app.py
+++ app/app.py
@@ -30,36 +30,37 @@ Base.prepare(engine, reflect=True)
 print('Tables: ',Base.classes.keys())
 
 # get planets
-session = Session(engine)
-planet_df = pd.read_sql_table('confirmed_planets1', engine)
-orbit_df = pd.read_sql_table('orbital_radius', engine)
-planet_df['Distance from Host Star'] = orbit_df['orbit_semi_major_axis']
-z = planet_df['host_name'].value_counts()
-z1 = z.to_dict()
-planet_df['Number of Planets in System'] = planet_df['host_name'].map(z1)
-planet_df
-conditions = conditions = [((planet_df['equilibrium_temperature'] >= 200) & (planet_df['equilibrium_temperature'] <= 320)
-               & (planet_df['planet_radius'] >= .5) & (planet_df['planet_radius'] <= 1.6)),
-              ((planet_df['equilibrium_temperature'] >= 200) & (planet_df['equilibrium_temperature'] <= 330)
-               & (planet_df['planet_radius'] > 1.6) & (planet_df['planet_radius'] <= 2.5)),
-              ((planet_df['equilibrium_temperature'] < 200) | (planet_df['equilibrium_temperature'] > 330)
-               | (planet_df['planet_radius'] < .5) | (planet_df['planet_radius'] > 2.5))]
-values = [2,1,0]
-planet_df['Habitability'] = np.select(conditions, values)
-planet_df = planet_df.drop(columns=['koi_name', 'exoplanet_archive_disposition', 'orbital_period',
-       'transit_epoch', 'impact_parameter', 'transit_duration',
-       'transit_depth', 'planetary_radius', 'insolation_flux',
-       'transit_signal_to_noise', 'tce_planet_number',
-       'stellar_surface_gravity', 'ra', 'decimal_degrees', 'kepler_band'], axis=1)
-print(planet_df.columns)
-planet_df.columns = ['Planet Name', 'Planet Mass', 'Planet Radius', 'Distance from Earth', 'Star Name', 'Equilibrium Temperature', 'Stellar Temperature', 'Stellar Radius', 'Distance from Host Star', 'Number of Planets in System', 'Habitability']
-planet_df.set_index('Planet Name')
-planet_df = planet_df
-#close_planet_df = planet_df.loc[planet_df['Distance from Earth'] <= 200]
+
+# session = Session(engine)
+# planet_df = pd.read_sql_table('confirmed_planets1', engine)
+# orbit_df = pd.read_sql_table('orbital_radius', engine)
+# planet_df['Distance from Host Star'] = orbit_df['orbit_semi_major_axis']
+# z = planet_df['host_name'].value_counts()
+# z1 = z.to_dict()
+# planet_df['Number of Planets in System'] = planet_df['host_name'].map(z1)
+# planet_df
+# conditions = conditions = [((planet_df['equilibrium_temperature'] >= 200) & (planet_df['equilibrium_temperature'] <= 320)
+#                & (planet_df['planet_radius'] >= .5) & (planet_df['planet_radius'] <= 1.6)),
+#               ((planet_df['equilibrium_temperature'] >= 200) & (planet_df['equilibrium_temperature'] <= 330)
+#                & (planet_df['planet_radius'] > 1.6) & (planet_df['planet_radius'] <= 2.5)),
+#               ((planet_df['equilibrium_temperature'] < 200) | (planet_df['equilibrium_temperature'] > 330)
+#                | (planet_df['planet_radius'] < .5) | (planet_df['planet_radius'] > 2.5))]
+# values = [2,1,0]
+# planet_df['Habitability'] = np.select(conditions, values)
+# planet_df = planet_df.drop(columns=['koi_name', 'exoplanet_archive_disposition', 'orbital_period',
+#        'transit_epoch', 'impact_parameter', 'transit_duration',
+#        'transit_depth', 'planetary_radius', 'insolation_flux',
+#        'transit_signal_to_noise', 'tce_planet_number',
+#        'stellar_surface_gravity', 'ra', 'decimal_degrees', 'kepler_band'], axis=1)
+# print(planet_df.columns)
+# planet_df.columns = ['Planet Name', 'Planet Mass', 'Planet Radius', 'Distance from Earth', 'Star Name', 'Equilibrium Temperature', 'Stellar Temperature', 'Stellar Radius', 'Distance from Host Star', 'Number of Planets in System', 'Habitability']
+# planet_df.set_index('Planet Name')
+# planet_df = planet_df
+
 
 # create instance of Flask app
 #app = Flask(__name__)
-db=SQLAlchemy(app)
+#db=SQLAlchemy(app)
 
 # here!! transfer
 @app.route('/')
@@ -80,9 +81,10 @@ def summary():
 
 @app.route('/api/close_planet_data')
 def planetAPI():
-    close_planet_json=planet_df.to_json(orient='columns', index='Planet Name')
+    #close_planet_json=planet_df.to_json(orient='columns', index='Planet Name')
 
-    return close_planet_json
+    #return close_planet_json
+    return("text")
 
 if __name__ == "__main__":
     app.run(debug=True)
